7407008718b285960c2aa02698c796a3dbd0abc1
Martin Andráši <andrasi.m007@gmail.com>
2020-12-09 23:25:12 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,377 @@
+<template>
+  <div>
+    <div class="cta-section">
+      <div class="menu">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 d-flex align-items-center">
+              <a href="/" class="menu-anchor">Martin Andráši</a>
+            </div>
+            <div
+              class="col-lg-6 d-lg-flex align-items-center justify-content-end d-none"
+            >
+              <a href="#cenik" class="menu-anchor menu-anchor--mr odkaz"
+                >Ceník</a
+              ><a href="#kontakt" class="button--orange odkaz">Kontakt</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div
+            class="jumbotron col-12 col-lg-10 offset-lg-1 col-xl-12 offset-xl-0"
+          >
+            <div class="texts">
+              <h1><span>Martin Andráši</span></h1>
+              <p>webový vývojář</p>
+            </div>
+
+            <div class="main-photo">
+              <img
+                class="img-responsive"
+                src="img/andrasi.png"
+                alt="andrasi martin"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <section class="whoiam">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-10 offset-lg-1">
+            <h2>Vývojář pro firmy</h2>
+            <p>
+              Jsem programátor zaměřený na webové technologie. Pomůžu vaší firmě
+              s technickou realizací webu, e-shopu nebo aplikace.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="experiences">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 offset-lg-1">
+            <div class="years">
+              <span class="number">4</span>
+              <span class="inline text-center">
+                <small>Roky <br />zkušeností</small>
+              </span>
+            </div>
+          </div>
+          <div class="col-lg-8">
+            <p>
+              Od malých<br />
+              začínajících firem až<br />
+              po ty nadnárodní.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="employer">
+      <div class="background"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 offset-lg-1 col-xl-2 offset-xl-2">
+            <h2>Pro koho pracuji</h2>
+          </div>
+          <div class="col-lg-6 offset-lg-2 col-xl-6 offset-xl-1">
+            <p class="cooperation">
+              Spolupracuji s firmami, které hledají spolehlivého a dlouhodobého
+              partnera
+            </p>
+          </div>
+          <div class="col-lg-8 offset-lg-1 col-xl-8 offset-xl-2">
+            <div class="row">
+              <div class="col-lg-8 col-xl-6">
+                <p class="description">
+                  V roli webového vývojáře jsem působil ve dvou firmách. V
+                  poslední době to byla firma Leksys, který se zabývá dodáváním
+                  kompletních online řešení a firma Brasty zabývající se
+                  prodejem hodinek nebo parfémů.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-7 offset-lg-5 d-flex flex-wrap logos">
+            <div class="card d-flex align-items-center">
+              <img src="img/brasty.svg" alt="brasty" />
+            </div>
+            <!--
+                <div class="card d-flex align-items-center">
+                    <img class="leksys" src="img/leksys.svg" alt="leksys">
+                </div>
+                -->
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="technology">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-10 offset-lg-1 col-xl-8 offset-xl-2 info-section">
+            <h2>Technologie</h2>
+            <p class="header">Zajistím kompletní technické řešení</p>
+            <p class="subheader">Ovládám backendovou i frontendovou část.</p>
+            <p class="description">
+              Zkušenosti v technickém vývoji webových řešení mám na obou
+              frontách. Dokážu pracovat s nejznámějšími databázemi MySQL nebo
+              PostgreSQL. Nejčastěji programuji v programovacím jazyce
+              Javascript v rámci Adonis.js nebo v jazyce PHP, kde používám
+              framework Laravel. Na frontendové straně se nebojím použít i
+              JavaScriptové frameworky jako je třeba Vue.js nebo Nuxt.js.
+            </p>
+          </div>
+
+          <div
+            class="fullstack-section col-lg-12 offset-lg-0 col-xl-8 offset-xl-2"
+          >
+            <div class="row">
+              <div class="col-lg-12">
+                <h2>Full-stack</h2>
+              </div>
+              <div class="col-sm-6 col-12 right-border-dashed">
+                <h3 class="header">Back-end</h3>
+                <ul>
+                  <li class="small">
+                    <div class="image">
+                      <img src="img/Logo/Technology/node.png" alt="Node.js" />
+                    </div>
+                    <b>Node.js - Adonis</b>
+                  </li>
+
+                  <li>
+                    <div class="image">
+                      <img src="img/Logo/Technology/PHP@1x.svg" alt="PHP" />
+                    </div>
+                    <b>PHP</b>Laravel, Symfony
+                  </li>
+
+                  <li class="small">
+                    <div class="image">
+                      <img
+                        src="img/Logo/Technology/Laravel@1x.svg"
+                        alt="Laravel"
+                      />
+                    </div>
+                    <b>Laravel</b>
+                  </li>
+
+                  <li class="small">
+                    <div class="image">
+                      <img
+                        src="img/Logo/Technology/Symfony@1x.svg"
+                        alt="Symfony"
+                      />
+                    </div>
+                    <b>Symfony</b>
+                  </li>
+
+                  <li>
+                    <div class="image">
+                      <img src="img/Logo/Technology/SQL@1x.svg" alt="SQL" />
+                    </div>
+                    <b>SQL</b>MySQL
+                  </li>
+
+                  <li class="small">
+                    <div class="image">
+                      <img src="img/Logo/Technology/MySQL@1x.svg" alt="Mysql" />
+                    </div>
+                    <b>MySQL</b>
+                  </li>
+                  <li class="small">
+                    <div class="image">
+                      <img
+                        src="img/Logo/Technology/PostgreSQL@1x.svg"
+                        alt="PostgreSQL"
+                      />
+                    </div>
+                    <b>PostgreSQL</b>
+                  </li>
+                </ul>
+              </div>
+
+              <div
+                class="col-sm-6 col-12 d-flex flex-column align-items-lg-end"
+              >
+                <div class="right">
+                  <h3 class="header">Front-end</h3>
+
+                  <ul class="d-flex flex-column align-items-lg-end">
+                    <li>
+                      <div class="image">
+                        <img
+                          src="img/Logo/Technology/JS@1x.svg"
+                          alt="Javascript"
+                        />
+                      </div>
+                      <b>Javascript</b>
+                    </li>
+
+                    <li>
+                      <div class="image">
+                        <img
+                          src="img/Logo/Technology/Vue@1x.svg"
+                          alt="Vue.js"
+                        />
+                      </div>
+                      <b>Vue.js</b>
+                    </li>
+
+                    <li>
+                      <div class="image">
+                        <img
+                          src="img/Logo/Technology/nuxt.svg"
+                          alt="Javascript"
+                        />
+                      </div>
+                      <b>Nuxt.js</b>
+                    </li>
+
+                    <li>
+                      <div class="image">
+                        <img src="img/Logo/Technology/HTML@1x.svg" alt="HTML" />
+                      </div>
+                      <b>HTML</b>
+                    </li>
+
+                    <li>
+                      <div class="image">
+                        <img src="img/Logo/Technology/CSS@1x.svg" alt="CSS" />
+                      </div>
+                      <b>CSS</b>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-10 offset-lg-1 contact">
+            <p>Získejte kompletní technické řešení pro váš webový projekt.</p>
+            <a href="#kontakt" class="odkaz button--orange">Kontaktujte mě</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="cenik" class="prices">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 offset-lg-3 col-xl-4 offset-xl-4 text-center">
+            <h2>Cena</h2>
+            <p class="header">Kolik stojí spolupráce se mnou?</p>
+            <p class="subheader">
+              Nabízím vám své zkušenosti ze dvou oborů. Místo dvou odborníků vám
+              postačí jeden.
+            </p>
+
+            <div class="price">
+              Cena za projekt nebo
+              <br />
+              800 Kč/hodina
+            </div>
+
+            <a href="#kontakt" class="button--orange odkaz">Kontaktujte mě</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="cooperation-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 offset-lg-3 text-center">
+            <h2>Hodnoty</h2>
+            <p class="header">
+              Mám rád spolupráce na zajímavých projektech,<br />co jsou pro mě
+              výzva
+            </p>
+            <div class="row">
+              <div class="col-lg-10 offset-lg-1">
+                <p class="description">
+                  Jsem otevřený novým a zajímavým projektům. Rád se učím nové
+                  technologie a překonávám překážky. Zakládám si na čestnosti,
+                  domluvě a upřímnosti. Rád se seznamuji s lidmi, kteří mi mají
+                  co předat. Mám zájem o partnery, kteří mě budou doporučovat a
+                  vyhledávat i po letech.
+                </p>
+              </div>
+            </div>
+
+            <div class="img">
+              <img src="img/andrasi-martin-logo.png" alt="andráši martin" />
+              <p class="name">Martin Andráši</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="kontakt" class="contact-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-10 offset-lg-1 text-center margin">
+            <h2>Kontakt</h2>
+            <p class="header">Spojme se a navažme spolupráci</p>
+          </div>
+
+          <div class="col-lg-8 offset-lg-2 col-xl-5 offset-xl-2">
+            <div class="card">
+              <a href="tel:+420604665567">+420 604 66 55 67</a>
+              <a href="mailto:martin@andrasi.cz">martin@andrasi.cz</a>
+              <hr />
+            </div>
+          </div>
+
+          <div class="col-sm-10 offset-sm-1 col-xl-3 offset-xl-1 social-sites">
+            <p>Najdete mě na sociálních sítích</p>
+            <div class="d-flex flex-wrap justify-content-between">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://twitter.com/MartinAndrasi"
+                ><img src="img/Logo/Social/Twiiter@1x.svg" alt="twitter"
+              /></a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/Guteres007"
+                ><img src="img/Logo/Social/Github@1x.svg" alt="github"
+              /></a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.instagram.com/andrasimartin"
+                ><img src="img/Logo/Social/Instagram@1x.svg" alt="intagram"
+              /></a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/martinandrasi/"
+                ><img src="img/Logo/Social/LinkedIn@1x.svg" alt="LinkedIn"
+              /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="footer text-center">
+      <p>Martin Andráši | 2020</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ stylelint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['stylelint-scss', 'stylelint-config-prettier'],
+
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {},
+}
