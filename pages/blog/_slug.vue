e4ef802a19de423933e136b007609708a06f04bf
Martin Andráši <andrasi.m007@gmail.com>
2020-12-17 19:45:01 +0100

    init blog

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      {{ article.slug }}
+      <article>
+        <nuxt-content :document="article" />
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'blog',
+  async asyncData({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch()
+
+    return { article }
+  },
+}
+</script>
